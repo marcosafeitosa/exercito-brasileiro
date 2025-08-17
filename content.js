70ebe8e9b13957aff2b96966f31a754dec2d912d
marcosafeitosa <alietoriando@gmail.com>
2025-08-17 10:33:18 -0300

    atualizacao

--- content.js
+++ content.js
@@ -3,5 +3,4 @@ import("./modules/botaoAcionador.js")
   .then((module) => module.verificarERodarModulo())
   .catch((err) => {
     console.error("Falha ao carregar módulo:", err);
-    exibirErroVisual("Falha ao carregar módulo: " + err.message);
   });
