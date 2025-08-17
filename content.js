539dae079fd5c4510d5c266ac9092132d9faea35
marcosafeitosa <alietoriando@gmail.com>
2025-08-17 11:42:28 -0300

    atualizacao

--- content.js
+++ content.js
@@ -51,7 +51,7 @@ function mostrarToastInstrucao() {
 
       <div style="display:flex; align-items:center; gap:8px;">
         <span style="font-size:18px;">⚡</span>
-        <span><strong>Pressione o atalho para injetar o script:</strong><br> Pressione <kbd>CTRL</kbd> + <kbd>SHIFT</kbd> + <kbd>E</kbd></span>
+        <span><strong>Atalho para injetar o script:</strong><br> Pressione <kbd>CTRL</kbd> + <kbd>SHIFT</kbd> + <kbd>E</kbd></span>
       </div>
     `;
 
